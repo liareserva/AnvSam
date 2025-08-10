066df22448434025b4f7602fe808e3b3611654df
liabersiv <juliaribeiro.dsv@gmail.com>
2025-08-10 17:13:22 -0300

    PROXIMA musica funcionando HAHAHAHAHAAHHAHA

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function playPause() {
         musica.play();
         btnMusic.classList.add('bxs-pause');
         btnMusic.classList.remove('bxs-play');
-        
+
         vinilCoracao.classList.add('rodando');
     };
 };
@@ -40,7 +40,7 @@ musica.addEventListener('timeupdate', () => {
     const max = progresso.max || 100;
     const x = ((progresso.value - min) / (max - min)) * 100;
     let cor = 'linear-gradient(90deg, rgb(209, 209, 209)' + x + '%, rgb(136, 136, 136)' + x + '%)';
-    
+
     progresso.style.background = cor;
 });
 
@@ -71,5 +71,54 @@ hiddenLetter('aLetter');
 hiddenLetter('mLetter2');
 hiddenLetter('oLetter');
 hiddenLetter('rLetter');
+
+
+
+
+
+
+
+
+
+const musicas = [
+    {
+        src: '../assets/audio/A$AP Rocky - Everyday (Official Audio) ft. Rod Stewart, Miguel, Mark Ronson.mp3',
+        nome: 'Everyday - A$AP Rocky'
+    },
+
+    {
+        src: '../assets/audio/Phantom Bride.mp3',
+        nome: 'Phantom Bride - Deftones'
+    }
+];
+
+function nextMusic() {
+
+    let musicaIndex = 0;
+    let audio = document.getElementById('musica');
+    const nomeMusica = document.getElementById('nameMusic');
+
+    const btnProximo = document.getElementById('proxIcon');
+
+    musicaIndex = 0;
+
+        musicaIndex++;
+        musicaAtual = musicaIndex;
+
+        audio.src = musicas[musicaAtual].src;
+        nomeMusica.textContent = musicas[musicaAtual].nome;
+
+        playPause();
+}
+
+
+
+
+
+
+
+
+
+
 
 
